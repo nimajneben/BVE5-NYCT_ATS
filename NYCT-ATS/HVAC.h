a9a79d0aca4929576acb3f8d166d7deb60fb93bb
Benjamin <47288387+nimajneben@users.noreply.github.com>
2020-05-25 16:28:08 -0400

    Update HVAC.h

--- NYCT-ATS/HVAC.h
+++ NYCT-ATS/HVAC.h
@@ -1,2 +1,15 @@
 //HVAC.h
 //Contains logic used to sound the cooling fan.
+#include<iostream>
+#include<fstream>
+
+std::ifstream tempfile("temp.txt"); //temperature file
+
+class NYCT_HVAC {
+public:
+	int NYCT_HVAC()
+	{
+		if (!tempfile.open)
+			std::cout << "Cannot open temperature file!";
+	}
+};
