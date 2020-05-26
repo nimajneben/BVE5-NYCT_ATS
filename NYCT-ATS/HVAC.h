c965f484fca555188ff869be3d8f61cdb6cba518
nimajneben <47288387+nimajneben@users.noreply.github.com>
2020-05-26 12:33:36 -0400

    Update HVAC.h

--- NYCT-ATS/HVAC.h
+++ NYCT-ATS/HVAC.h
@@ -6,10 +6,22 @@
 std::ifstream tempfile("temp.txt"); //temperature file
 
 class NYCT_HVAC {
+private:
+	int bg_temp;	//ambient temperature
+	int rate_frictionbr;	//heating rate when friction brakes are applied (>1)
+	int rate_dynamicbr;		//heating rate when dynamic brakes are applied (>1)
+	int temperature;	//what the thermistor reads
 public:
-	int NYCT_HVAC()
+	bool active;	//HVAC status
+	bool InitHVAC() //Init HVAC
 	{
-		if (!tempfile.open)
-			std::cout << "Cannot open temperature file!";
+		if (!tempfile.open) return false;
+		temperature = bg_temp;
+		return true;
+	}
+
+	void RunHVAC()
+	{
+
 	}
 };
